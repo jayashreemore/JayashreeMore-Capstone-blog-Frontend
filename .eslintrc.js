aa677fb5dd20315957d8e43368280d5aa7887a30
jayashreemore <120532991+jayashreemore@users.noreply.github.com>
2024-04-29 22:58:23 -0400

    warning fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   // Other ESLint configurations...
   rules: {
     // Other rules...
-    "no-unused-vars": process.env.CI ? "warn" : "error",
+    "no-unused-vars": "warn", // Treat unused variables as warnings
   },
 };
